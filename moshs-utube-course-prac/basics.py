9c941ac722cd60eb9ac694d7d3012317d668c48d
Suneel Sonti <suneel_sonti@epam.com>
2021-06-12 14:35:23 +0530

    Asdded basics from Mosh's youtube course

---
+++ moshs-utube-course-prac/basics.py
@@ -0,0 +1,25 @@
+#print("hello world")
+
+"""
+lamb = 'mutton'
+print("would you like to have" + lamb)
+"""
+'''==========
+=============='''
+'''
+##The below block will take two variables as input and then convert the second variable to print in lowercase
+
+name = input("What is your name ?" '\n')
+
+color = input("what is your favorite color ?" '\n')
+
+colour = color.lower()
+
+print('\n')
+
+print(name+"'s" + " favorite color is " + colour)
+'''
+'''==========
+=============='''
+
+
